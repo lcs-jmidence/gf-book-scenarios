becb21d96501ae5ce3954cff31ed88505bff236b
Julio Midence <jmidence@lcs.on.ca>
2019-04-11 09:52:54 -0400

    Declared an array of names

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -13,6 +13,10 @@ public class Piano extends World
      * (we can use the variables anywhere in the class)
      */
     int frames;
+    
+    // Create the array (list of names)
+    String[] peopleInClass = {"Julio", "Liam", "Dima", "Osman", "Nick", "Simon", "Eric", "Phillip", 
+        "Joven", "Joe", "Gordon", "Jieun", "Selena", "Jason", "Sam", "Jeff"};
     public Piano() 
     {
         super(800, 340, 1);
